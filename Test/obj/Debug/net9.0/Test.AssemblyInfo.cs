73430d35cc1d3252668f9bf44348b44c6f56d40c
Laura Codutti <agostinacod@gmail.com>
2026-03-27 22:10:02 -0300

    cambio

--- Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net9.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f7542787a533c0e0efbdeb3599a8ac227cad2d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90afcee56585648c8c59aeb24aaa589e42d33327")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
